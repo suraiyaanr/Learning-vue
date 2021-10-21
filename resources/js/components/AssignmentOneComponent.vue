60b4b406bb162f39819a0a78f4fcd98599afc176
Suraiya Anuar <suraiyaanuar@gmail.com>
2021-10-21 18:15:14 +0800

    first commit

---
+++ resources/js/components/AssignmentOneComponent.vue
@@ -0,0 +1,24 @@
+<template>
+	<span>
+		<div>
+			<p>{{ message }}</p>
+              <button v-on:click="reverseMessage" class="btn btn-block btn-success">Click Reverse String</button>
+          
+		</div>
+	</span>
+</template>
+
+<script>  
+export default {
+		data() {
+			return {
+				message: 'Saya sedang belajar VueJS'
+			}
+		},
+		methods: {
+			reverseMessage: function () {
+			this.message = this.message.split('').reverse().join('')
+			}
+  }
+	}
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<!-- # Attach di sini -->
+	<div id="app">
+		<!-- <example-component></example-component> -->
+        <assignment-one-component></assignment-one-component>
+	</div>
+
+	<!-- # Untuk load file yang telah di compile public/js/app.js -->
+	<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html> 
